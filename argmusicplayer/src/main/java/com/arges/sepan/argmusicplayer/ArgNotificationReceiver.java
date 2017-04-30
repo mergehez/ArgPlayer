8d2a7d3544caf451b70342249a755450494d1c33
MergeSoft <argesozdogan@gmail.com>
2017-04-30 18:18:07 +0300

    Update ArgNotificationReceiver.java

--- argmusicplayer/src/main/java/com/arges/sepan/argmusicplayer/ArgNotificationReceiver.java
+++ argmusicplayer/src/main/java/com/arges/sepan/argmusicplayer/ArgNotificationReceiver.java
@@ -3,12 +3,7 @@ package com.arges.sepan.argmusicplayer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.arges.sepan.argmusicplayer.IndependentClasses.Arg;
-
 
 public class ArgNotificationReceiver extends BroadcastReceiver{
     public ArgNotificationReceiver(){}
@@ -16,39 +11,25 @@ public class ArgNotificationReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent){
         String action = intent.getAction();
         ArgMusicPlayer player = ArgMusicPlayer.getInstance();
-        Intent serviceIntent;
+        if(player == null){
+            ArgNotification.close(context);
+            return;
+        }
+        Log.d("ARGCIH", "NotifHelper onReceive action: "+action);
         switch (action){
             case "com.arges.intent.PLAYPAUSE":
-                Log.d("ARGCIH", "NotifHelper onReceive action: "+action);
                 if(player.isPlaying())player.pause();
                 else player.continuePlaying();
                 break;
             case "com.arges.intent.CLOSE":
-                ArgNotification.close();
-                try{
-                    player.stop();
-                }catch (Exception ec){Log.d("Notification Close"," Cannot stop music");}
-
+                ArgNotification.close(context);
+                player.stop();
                 break;
             case "com.arges.intent.NEXT":
-                Log.d("ARGCIH", "NotifHelper onReceive action: "+action);
                 player.playNextAudio();
                 break;
             case "com.arges.intent.PREV":
-                Log.d("ARGCIH", "NotifHelper onReceive action: "+action);
                 player.playPrevAudio();
-                break;
-            case "com.arges.intent.HOME":
-                Log.d("ARGCIH", "NotifHelper onReceive action: "+action);
-                Intent i = new Intent(context, ArgPlayerViewRoot.mainActivityClass);
-                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                }
-                context.startActivity(i);
-                Toast.makeText(context,"Sepan vebû",Toast.LENGTH_SHORT).show();
-                Arg.hideNotifPanel(context);
-                Arg.unlockScreen(context);
                 break;
         }
     }
